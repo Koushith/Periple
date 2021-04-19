a86419756e4957c100395c1a0670f1ab6def3b07
koushith <koushith97@gmail.com>
2021-04-20 00:23:08 +0530

    adds seed data

---
+++ frontend/posts.js
@@ -0,0 +1,15 @@
+const posts = [
+  {
+    _id: '1',
+    name: 'A day to be remembered',
+    publishedAt: new Date().toDateString(),
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+
+    location: 'Bangalore',
+    mood: 'happy',
+  },
+];
+
+export default posts;
